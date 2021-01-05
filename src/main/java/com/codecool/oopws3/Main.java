533867ac5e5b3fa2aae4c78184ec4cf968f09380
Adam Hamar <adam.hamar@gmail.com>
2021-01-05 17:58:35 +0100

    Test the Owner class

--- src/main/java/com/codecool/oopws3/Main.java
+++ src/main/java/com/codecool/oopws3/Main.java
@@ -1,7 +1,14 @@
 package com.codecool.oopws3;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-    
+        List<Animal> animals = List.of(
+            new Dog("Csibész", true),
+            new Cat("Falánk", false)
+        );
+        Owner owner = new Owner(animals);
+        owner.feed();
     }
 }
